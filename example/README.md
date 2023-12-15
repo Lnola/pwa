c3e2f0b1e2240817ee0e6f18c7af1011f3fefb1c
Lnola <nola.luka@gmail.com>
2023-12-15 22:54:08 +0100

    :fire: Remove the example files

--- example/README.md
+++
@@ -1,2 +0,0 @@
-# pwa
-Progressive web app built for FER web2 course - 2023/2024
--- example/js/ui.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const menus = document.querySelectorAll('.side-menu');
-  M.Sidenav.init(menus, { edge: 'right' });
-});
