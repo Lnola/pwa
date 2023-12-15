0479ea58c0c4baf75d1732973704b2adb2c85efa
Lnola <nola.luka@gmail.com>
2023-12-16 00:12:38 +0100

    :sparkles: Add the custom links

--- client/scripts/native-api.js
+++ client/scripts/native-api.js
@@ -29,3 +29,21 @@ const nativeShare = async () => {
 const openDialog = navigator.share ? nativeShare : openFallbackDialog;
 shareButton.addEventListener('click', openDialog);
 closeButton.addEventListener('click', closeFallbackDialog);
+
+const twitter = document.querySelector('.twitter');
+twitter.href = `https://twitter.com/share?&url=${data.text}%0A${data.text}%0A${data.url}`;
+const facebook = document.querySelector('.facebook');
+facebook.href = `http://www.facebook.com/share.php?u=${data.url}`;
+const email = document.querySelector('.email');
+email.href = `mailto:?subject=${data.title}&body=${data.text}%0A${data.url}`;
+const linkedin = document.querySelector('.linkedin');
+linkedin.href = `https://www.linkedin.com/shareArticle?mini=true&url=${data.url}`;
+const url = document.querySelector('.url');
+url.innerHTML = data.url;
+
+const copyLinkButton = document.querySelector('.copy-url');
+const copyUrlToClipboard = () => {
+  navigator.clipboard.writeText(data.url);
+  copyLinkButton.innerHTML = 'Copied!';
+};
+copyLinkButton.addEventListener('click', copyUrlToClipboard);
