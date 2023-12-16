2cce4d9431d6f47e7d9e0c5f9a39a2ea94da08f4
Lnola <nola.luka@gmail.com>
2023-12-16 03:16:20 +0100

    :sparkles: Create a static cache saving

--- client/sw.js
+++ client/sw.js
@@ -1,11 +1,23 @@
+const STATIC_CACHE_NAME = 'site-static-v1';
+const STATIC_ASSETS = [
+  '/client',
+  '/client/index.html',
+  '/client/assets/css/layout.css',
+  '/client/assets/css/products.css',
+  '/client/scripts/index.js',
+  '/client/scripts/layout.js',
+  '/client/scripts/products.js',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css',
+  'https://fonts.gstatic.com/s/materialicons/v140/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+  'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js',
+];
+
 self.addEventListener('install', event => {
-  console.log('Installed Service Worker', event);
-});
-
-self.addEventListener('activate', event => {
-  console.log('Activated Service Worker', event);
-});
-
-self.addEventListener('fetch', event => {
-  console.log('Caught Fetch Event', event);
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(STATIC_CACHE_NAME);
+      return cache.addAll(STATIC_ASSETS);
+    })(),
+  );
 });
