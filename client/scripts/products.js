63e7886c96b48a22df164bc52cee171dae091e8d
Lnola <nola.luka@gmail.com>
2023-12-16 01:07:23 +0100

    :sparkles: Fetch products from an api

--- client/scripts/products.js
+++ client/scripts/products.js
@@ -1,21 +1,23 @@
-const data = {
-  imageUrl: 'https://flowbite.com/docs/images/blog/image-1.jpg',
-  title: 'Noteworthy technology acquisitions 2021',
-  description:
-    'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
+const getProducts = () => {
+  return fetch('https://fakestoreapi.com/products').then(res => res.json());
 };
 
-const createCardClone = (template, { imageUrl, title, description }) => {
+const createCardClone = (template, { image, price, title, description }) => {
   const clone = template.cloneNode(true);
-  clone.querySelector('.card-image').src = imageUrl;
+  clone.querySelector('.card-image').src = image;
   clone.querySelector('.card-title').textContent = title;
   clone.querySelector('.card-description').textContent = description;
+  clone.querySelector('.card-price').textContent = price + '€';
   return clone;
 };
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
   const template = document.getElementById('card-template').content;
   const container = document.getElementById('card-container');
-  const clone = createCardClone(template, data);
-  container.appendChild(clone);
+
+  const products = await getProducts();
+  products.forEach(product => {
+    const clone = createCardClone(template, product);
+    container.appendChild(clone);
+  });
 });
